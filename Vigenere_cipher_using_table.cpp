ff4121c93df7d68d70dfca79790926c30c63b689
MuhammadAsadullah <111900738+Mumdasadullah@users.noreply.github.com>
2024-02-10 01:34:17 +0500

    Update Vigenere_cipher_using_table.cpp

--- Vigenere_cipher_using_table.cpp
+++ Vigenere_cipher_using_table.cpp
@@ -46,10 +46,41 @@ string encryption(string input, string key, char tab[][26])
     return cipher_text;
 }
 
+string decryption(string input, string key, char tab[][26])
+{
+    char arr[2][input.length()];
+    int j = 0;
+    int row, col;
+    string cipher_text;
+    
+    for(int i=0; i<input.length(); i++)
+    {
+        arr[0][i] = input[i];
+        arr[1][i] = key[j];
+        j++;
+        if(j == key.length())
+        {
+            j = 0;
+        }
+        row = int(arr[1][i]) - 65;
+        for(int j=0; j<26; j++)
+        {
+            if(tab[row][j] == arr[0][i])
+            {
+                col = j;
+                cipher_text += tab[0][col];
+                break;
+            }
+        }
+    }
+    return cipher_text;
+}
+
 int main() {
     // Write C++ code here
     cout << "Hello world!" << endl;
-    
+    int n;
+    string output;
     char table[26][26];
     for(int i=0; i<26; i++)
     {
@@ -65,12 +96,21 @@ int main() {
     
     string text = user_plain_text_input();
     string shift_key = user_keyword_input();
-    string output = encryption(text,shift_key,table);
-    cout << endl;
-    cout << "Cipher Text is : " << output;
-
-    cout << endl;
     
+    cout << "Encryption(1) or Decryption(2) : ";
+    cin >> n;
+    if(n == 1)
+    {
+        output = encryption(text,shift_key,table);
+    }
+    else
+    {
+        output = decryption(text,shift_key,table);
+    }
+    cout << endl;
+    cout << "Output is : " << output << endl;
+    cout << endl;
+    cout << "Table is : " << endl;
     for(int i=0; i<26; i++)
     {
         for(int j=0; j<26; j++)
