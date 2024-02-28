07dfee6b5a9e96b424ec80890effa726ae4deca9
MuhammadAsadullah <111900738+Mumdasadullah@users.noreply.github.com>
2024-02-28 20:46:13 +0500

    Update Playfair_Cipher.cpp

--- Playfair_Cipher.cpp
+++ Playfair_Cipher.cpp
@@ -1,4 +1,12 @@
+def user_input():
+    text = str(input("Enter Plain Text : "))
+    keys = str(input("Enter Key : "))
+    text = text.replace(" ","")
+    return text,keys
+
 def table(key):
+    tab = []
+    val = []
     lst = ['A','B','C','D','E','F','G','H','I','K','L','M','N','O','P',
             'Q','R','S','T','U','V','W','X','Y','Z']
     for i in range(len(key)):
@@ -7,23 +15,25 @@ def table(key):
                 if lst[j] == key[i]:
                     break
             lst.pop(j)
-    tab = []
-    val = []
-    for i in range (len(key)):
         if i==5:
             tab.append(val)
             val = []
         val.append(key[i])
-    # k = len(val)
+    # for i in range (len(key)):
+    #     if i==5:
+    #         tab.append(val)
+    #         val = []
+    #     val.append(key[i])
     if len(val) > 0 and len(val) < 5:
         for i in lst:
             val.append(i)
             if (len(val) == 5):
                 tab.append(val)
                 val = []
-    # tab.append(val)
-    print(tab)
+    return tab
 
 if __name__ == '__main__':
-    key = str(input("Enter Key : "))
-    table(key)
+    plain_text,key = user_input()
+    # key = str(input("Enter Key : "))
+    mat = table(key)
+    print(mat)
