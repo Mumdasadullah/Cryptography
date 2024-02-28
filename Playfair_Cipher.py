c30104c65f21de63ff426d55095271f4ecc9ca43
MuhammadAsadullah <111900738+Mumdasadullah@users.noreply.github.com>
2024-02-29 03:15:03 +0500

    Update Playfair_Cipher.py

--- Playfair_Cipher.py
+++ Playfair_Cipher.py
@@ -19,7 +19,7 @@ def pair_text(text):
         pair.append(p)
         p = []
     
-    print(pair)
+    return pair
 
 def table(key):
     tab = []
@@ -44,9 +44,126 @@ def table(key):
                 val = []
     return tab
 
+def display_table(tabl):
+    for i in range(5):
+        for j in range(5):
+            print(tabl[i][j], end=" ")
+        print()
+
+def rule(pr, matrix):
+    res = []
+    v = []
+    # display_table(matrix)
+    for i, row in enumerate(matrix):
+        if pr[0] in row:
+            v.append(i)
+            v.append(row.index(pr[0]))
+            break
+    res.append(v)
+    v = []
+    for i, row in enumerate(matrix):
+        if pr[1] in row:
+            v.append(i)
+            v.append(row.index(pr[1]))
+            break
+    res.append(v)
+    return res
+    
+def encryption(pr, matrix):
+    tt = []
+    ttt = []
+    for i in pr:
+        l = rule(i, matrix)
+        ttt.append(rule(i, matrix))
+        l1,l2 = l[0], l[1]
+        if l1[0] == l2[0]:
+            if l1[1] != 4 and l2[1] != 4:
+                tt.append(matrix[l1[0]][l1[1]+1])
+                tt.append(matrix[l2[0]][l2[1]+1])
+            elif l1[1] == 4 and l2[1] != 4:
+                tt.append(matrix[l1[0]][0])
+                tt.append(matrix[l2[0]][l2[1]+1])
+            elif l2[1] == 4 and l1[1] != 4:
+                tt.append(matrix[l1[0]][l1[1]+1])
+                tt.append(matrix[l2[0]][0])
+            else:
+                tt.append(matrix[l1[0]][0])
+                tt.append(matrix[l2[0]][0])
+        elif l1[1] == l2[1]:
+            if l1[0] != 4 and l2[0] != 4:
+                tt.append(matrix[l1[0]+1][l1[1]])
+                tt.append(matrix[l2[0]+1][l2[1]])
+            elif l1[0] == 4 and l2[0] != 4:
+                tt.append(matrix[0][l1[1]])
+                tt.append(matrix[l2[0]+1][l2[1]])
+            elif l1[0] != 4 and l2[0] == 4:
+                tt.append(matrix[l1[0]+1][l1[1]])
+                tt.append(matrix[0][l2[1]])
+            else:
+                tt.append(matrix[0][l1[1]])
+                tt.append(matrix[0][l2[1]])
+        else:
+            tt.append(matrix[l1[0]][l2[1]])
+            tt.append(matrix[l2[0]][l1[1]])
+    
+    return tt
+
+def decryption(pr, matrix):
+    tt = []
+    ttt = []
+    for i in pr:
+        l = rule(i, matrix)
+        ttt.append(rule(i, matrix))
+        l1,l2 = l[0], l[1]
+        if l1[0] == l2[0]:
+            if l1[1] != 4 and l2[1] != 4:
+                tt.append(matrix[l1[0]][l1[1]-1])
+                tt.append(matrix[l2[0]][l2[1]-1])
+            elif l1[1] == 4 and l2[1] != 4:
+                tt.append(matrix[l1[0]][l1[1]-1])
+                tt.append(matrix[l2[0]][l2[1]-1])
+            elif l2[1] == 4 and l1[1] != 4:
+                tt.append(matrix[l1[0]][l1[1]-1])
+                tt.append(matrix[l2[0]][0])
+            else:
+                tt.append(matrix[l1[0]][0])
+                tt.append(matrix[l2[0]][0])
+        elif l1[1] == l2[1]:
+            if l1[0] != 4 and l2[0] != 4:
+                tt.append(matrix[l1[0]-1][l1[1]])
+                tt.append(matrix[l2[0]-1][l2[1]])
+            elif l1[0] == 4 and l2[0] != 4:
+                tt.append(matrix[0][l1[1]])
+                tt.append(matrix[l2[0]-1][l2[1]])
+            elif l1[0] != 4 and l2[0] == 4:
+                tt.append(matrix[l1[0]-1][l1[1]])
+                tt.append(matrix[0][l2[1]])
+            else:
+                tt.append(matrix[0][l1[1]])
+                tt.append(matrix[0][l2[1]])
+        else:
+            tt.append(matrix[l1[0]][l2[1]])
+            tt.append(matrix[l2[0]][l1[1]])
+    
+    return tt
+    
+def result(tt):
+    for i in range (len(tt)):
+        print(tt[i], end="")
+    print()
+
 if __name__ == '__main__':
     plain_text,key = user_input()
     # key = str(input("Enter Key : "))
     mat = table(key)
-    pair_text(plain_text)
+    plain_pair = pair_text(plain_text)
+    display_table(mat)
+    # output = encryption(plain_pair, mat)
+    n = int(input("Encryption (1) or Decryption (2) : "))
+    if n == 1:
+        output = encryption(plain_pair, mat)
+    else:
+        output = decryption(plain_pair, mat)
+    print("The Output Text is : ", end=" ")
+    result(output)
     # print(mat)
