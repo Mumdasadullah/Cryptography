75e3f7eb53258de1829d1ae9e16b8ce5a5c839ae
MuhammadAsadullah <111900738+Mumdasadullah@users.noreply.github.com>
2024-03-06 19:34:40 +0500

    Update Morse_code.py

--- Morse_code.py
+++ Morse_code.py
@@ -5,55 +5,35 @@ def userInput():
     return plain_text
     
 def morseCodeTable():
-    table = {'A': '.-',
-             'B':'-...',
-             'C':'-.-.',
-             'D':'-..',
-             'E':'.',
-             'F':'..-.',
-             'G':'--.',
-             'H':'....',
-             'I':'..',
-             'J':'.---',
-             'K':'-.-',
-             'L':'.-..',
-             'M':'--',
-             'N':'-.',
-             'O':'---',
-             'P':'.--.',
-             'Q':'--.-',
-             'R':'.-.',
-             'S':'...',
-             'T':'-',
-             'U':'..-',
-             'V':'...-',
-             'W':'.--',
-             'X':'-..-',
-             'Y':'-.--',
-             'Z':'--.',
-             '0':'-----',
-             '1':'.----',
-             '2':'..---',
-             '3':'...--',
-             '4':'....-',
-             '5':'.....',
-             '6':'-....',
-             '7':'--...',
-             '8':'---..',
-             '9':'----.'
+    table = {'A': '.-','B':'-...','C':'-.-.','D':'-..','E':'.','F':'..-.','G':'--.','H':'....','I':'..','J':'.---','K':'-.-','L':'.-..','M':'--','N':'-.','O':'---','P':'.--.','Q':'--.-','R':'.-.','S':'...','T':'-','U':'..-','V':'...-','W':'.--','X':'-..-','Y':'-.--','Z':'--..','0':'-----','1':'.----','2':'..---','3':'...--','4':'....-','5':'.....','6':'-....','7':'--...','8':'---..','9':'----.'
     }
     return table
     
-def result(text, table):
+def encryption(text, table):
     res = ""
     for i in text:
         for j in table:
             if i == j:
-                res += table[j]
+                res += table[j] + " "
+    
+    return res
+    
+def decryption(text, table):
+    res = ""
+    for i in text:
+        for j in table:
+            if i == table[j]:
+                res += j
     
     return res
 if __name__ == '__main__':
-    text = userInput()
     tab = morseCodeTable()
-    output = result(text.upper(),tab)
+    n = int(input("Encryption(1) or Decryption(2) : "))
+    if n == 1:
+        text = userInput()
+        output = encryption(text.upper(),tab)
+    else:
+        text = str(input("Enter Morse Code with gap : "))
+        # print(text)
+        output = decryption(text.split(" "),tab)
     print(output)
