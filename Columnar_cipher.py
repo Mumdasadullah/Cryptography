62c546ce8ca5e110b2c89643305f04b7cee07d3c
MuhammadAsadullah <111900738+Mumdasadullah@users.noreply.github.com>
2024-03-27 16:55:57 +0500

    Update Columnar_cipher.py

--- Columnar_cipher.py
+++ Columnar_cipher.py
@@ -1,10 +1,10 @@
 # Online Python compiler (interpreter) to run Python online.
 # Write Python 3 code in this online editor and run it.
 def user_input():
-    # plain_text = str(input("Enter Plain Text : "))
-    plain_text = "e--kefGsGsrekoe"
-    # key = str(input("Enter Key : "))
-    key = "HACK"
+    plain_text = str(input("Enter Plain Text : "))
+    # plain_text = "e--kefGsGsrekoe"
+    key = str(input("Enter Key : "))
+    # key = "HACK"
     return plain_text,key
     
 def display(table, key):
@@ -82,44 +82,6 @@ def create_table_dec(text, key):
             table[j][ind] = text[i]
             i+=1
     return table
-    # print(table)
-    # res = []
-    # order = []
-    # o = 1
-    # for i in range(len(key)):
-    #     res.append(key[i])
-    # table.append(res)
-    # res = []
-    # for i in table[0]:
-    #     for j in range(65,90):
-    #         if i == chr(j):
-    #             res.append(j)
-    # for i in range(len(table[0])):
-    #     ind = res.index(min(res))
-    #     order.append(ind)
-    #     res[ind] = 1000
-    # table.append(res)
-    # res = []
-    # for i in range(len(table[0])):
-    #     table[1][order[i]] = o
-    #     o+=1
-    
-    # # for i in range(len(text)):
-    # #     if i != 0:
-    # #         if i % len(key) == 0:
-    # #             table.append(res)
-    # #             res = []
-    # #     if text[i] == ' ':
-    # #         res.append('-')
-    # #     else:
-    # #         res.append(text[i])
-    # # if len(res) != len(key):
-    # #     for i in range (len(res),len(key)):
-    # #         res.append('-')
-    # table.append(res)
-    # # print(table)
-    # # display(table, key)
-    # return table
     
 def encryption(table):
     cipher_text = ""
@@ -146,7 +108,11 @@ def decryption(table):
     
 if __name__ == "__main__":
     text,shift_key = user_input()
-    print(f'the text is {text} and shift key is {shift_key}')
-    tab = create_table_dec(text, shift_key)
-    # encryption(tab)
-    decryption(tab)
+    # print(f'the text is {text} and shift key is {shift_key}')
+    n = int(input("Encryption(1) or Decryption(2) : "))
+    if n == 1:
+        tab = create_table_enc(text, shift_key)
+        encryption(tab)
+    else:
+        tab = create_table_dec(text, shift_key)
+        decryption(tab)
